1463f95496c68b2e69eef5cb090b895c24451eb6
Gabriela Freitas de albuquerque <gafreita@c2r9s3.42lisboa.com>
2023-02-07 22:04:43 +0000

    making changes to includes on Makefile and removing submodule

--- sources/libft.c
+++ sources/libft.c
@@ -1,20 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   libft.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gafreita <gafreita@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/02/01 19:21:40 by gafreita          #+#    #+#             */
-/*   Updated: 2023/02/07 21:57:00 by gafreita         ###   ########.fr       */
+/*   Created: 2023/02/07 20:28:57 by gafreita          #+#    #+#             */
+/*   Updated: 2023/02/07 21:54:50 by gafreita         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
-
-int	main()
-{
-	t_data data;
-
-	parse_map(&data, "/nfs/homes/dmendonc/cub3d/maps/firstmap.cub");
-}
